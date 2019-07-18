882ac9b67fb6405185f4c406a8839badd77f2dc5
Varis Darasirikul <varis.dara@gmail.com>
2019-07-18 22:52:56 +0800

    Add decrement button

--- counter.test.js
+++ counter.test.js
@@ -6,6 +6,22 @@ describe('Counter component', () => {
   it('starts with a count of 0', () => {
     const wrapper = shallow(<Counter />)
     const text = wrapper.find('p').text()
+    expect(text).toEqual('Current count: 0')
+  })
+
+  it('can increment the count when the button is clicked', () => {
+    const wrapper = shallow(<Counter />)
+    const incrementBtn = wrapper.find('button').first()
+    incrementBtn.simulate('click')
+    const text = wrapper.find('p').text()
     expect(text).toEqual('Current count: 1')
+  })
+
+  it('can decrement the count when the button is clicked', () => {
+    const wrapper = shallow(<Counter />)
+    const incrementBtn = wrapper.find('button.decrement')
+    incrementBtn.simulate('click')
+    const text = wrapper.find('p').text()
+    expect(text).toEqual('Current count: -1')
   })
 })
